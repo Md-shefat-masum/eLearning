7828e5d761d41bfe05ca35f522bf1897f377b627
hasan1119 <hasan63251119@gmail.com>
2023-05-17 00:39:09 +0600

    ok

--- src/views/frontend/home/webDesignModules.js
+++ src/views/frontend/home/webDesignModules.js
@@ -1,80 +1,30 @@
 export default [    
    {
-    id: "1",
+    id: "১",
     name:"Build a Personal Portfolio & Deployment",
     modules:[
-        {
-            id: '1',
-            name: "Introduction, Web & Internet Fundamentals",
-            topics:[
-                "Network",
-                "Internet",
-                "Server, Domain, IP & PORT",
-                "DNS",
-                "HTTP",
-                "Web",
-                "Website vs Web Application"
-            ]
-        },
-        {
-            id: '2',
-            name: "HTML, Everything needs to be learned",
-            topics:[
-                "Getting introduced to HTML",
-                "Syntax, Tags,Attributes, Comments & Document Structure",
-                "Heading Tags (h1 - h6)",
-                "Paragraphs (p)",
-                "Links (a)",
-                "Images (img, picture)",
-                "Formatting Elements (b, strong, i, em, mark, small, del, ins, sub, sup)",
-                "Quotation and Citation Elements (blockquote, q, abbr, address, cite)",
-                "Favicon & Meta Tags",
-                "Lists",
-                "Tables",
-                "Video, Audio, iFrames",
-                "Form & Input",
-                "Symantic & Inline Vs Block Tags"
-            ]
-        },
+           {
+            id: "১",
+            name: "Module:Introduction, Web & Internet Fundamentals"
+           },
+           {
+            id: "২",
+            name: "HTML, Everything needs to be learned"
+           },
     ]
    },
    {
-    id: "1",
-    name:"Build a Personal Portfolio & Deployment",
+    id: "২",
+    name:"PSD TO HTML",
     modules:[
-        {
-            id: '1',
-            name: "Introduction, Web & Internet Fundamentals",
-            topics:[
-                "Network",
-                "Internet",
-                "Server, Domain, IP & PORT",
-                "DNS",
-                "HTTP",
-                "Web",
-                "Website vs Web Application"
-            ]
-        },
-        {
-            id: '2',
-            name: "HTML, Everything needs to be learned",
-            topics:[
-                "Getting introduced to HTML",
-                "Syntax, Tags,Attributes, Comments & Document Structure",
-                "Heading Tags (h1 - h6)",
-                "Paragraphs (p)",
-                "Links (a)",
-                "Images (img, picture)",
-                "Formatting Elements (b, strong, i, em, mark, small, del, ins, sub, sup)",
-                "Quotation and Citation Elements (blockquote, q, abbr, address, cite)",
-                "Favicon & Meta Tags",
-                "Lists",
-                "Tables",
-                "Video, Audio, iFrames",
-                "Form & Input",
-                "Symantic & Inline Vs Block Tags"
-            ]
-        },
+           {
+            id: "১",
+            name: "Module:Introduction, Web & Internet Fundamentals"
+           },
+           {
+            id: "২",
+            name: "HTML, Everything needs to be learned"
+           },
     ]
    }
 ];
