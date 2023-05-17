68c73fcf93bdae2e493345809951fe2604082ef3
ruhulamin77 <ruhulaminbd93@gmail.com>
2023-05-17 23:50:34 +0600

    proof reading

--- src/views/frontend/index.vue
+++ src/views/frontend/index.vue
@@ -4,7 +4,7 @@
 		<Features></Features>
 		<CourseSegments></CourseSegments>
 		<ClassBasedCourseModules></ClassBasedCourseModules>
-		<Projects></Projects>
+		<!-- <Projects></Projects> -->
 		<WhatYouWillLearn></WhatYouWillLearn>
 		<free-course></free-course>
 		<HowCourseWillRun></HowCourseWillRun>
@@ -19,7 +19,7 @@ import Banner from "./home/banner.vue";
 import Features from "./home/features.vue";
 import CourseSegments from "./home/courseSegments.vue";
 import ClassBasedCourseModules from "./home/classBasedCourseModules.vue";
-import Projects from "./home/projects.vue";
+// import Projects from "./home/projects.vue";
 import HowCourseWillRun from "./home/howCourseWillRun.vue";
 import Faq from "./home/faq.vue";
 import Contact from "./home/contact.vue";
@@ -33,7 +33,7 @@ export default {
 		CourseSegments,
 		Instructor,
 		ClassBasedCourseModules,
-		Projects,
+		// Projects,
 		WhatYouWillLearn,
 		HowCourseWillRun,
 		FreeCourse,
